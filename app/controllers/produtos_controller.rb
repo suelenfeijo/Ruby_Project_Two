db4aed2861fe49c782e9ce12cec9174b0163ff9b
suelenfeijo <suelenfeijobr@gmail.com>
2024-02-05 08:14:35 -0300

    add método para permissão de chegada da requisição via body produto_params

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -11,8 +11,18 @@ class ProdutosController < ApplicationController
         @produto = Produto.new
     end
 
+    def create
+        @produto = Produto.new(produto_params)
+    end
+
     private
     def setProduto
         @produto = Produto.find(params[:id])
+    end
+
+    def produto_params
+    #método que auxiliar a requerer , e conceder a permissão para que o
+    #controller de create receba os campos de new produto e os salve no método post create
+        params.require(:produto).permit(:name, :quantidade, :price)
     end
 end
