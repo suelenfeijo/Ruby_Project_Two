c6180b6b0d00c8594cb09e555179bd35fe206b12
suelenfeijo <suelenfeijobr@gmail.com>
2024-02-05 01:49:33 -0300

    add método auxiliar da callBack de ProdutosController - setProduto

--- app/controllers/produtos_controller.rb
+++ app/controllers/produtos_controller.rb
@@ -1,9 +1,13 @@
 class ProdutosController < ApplicationController
+
     def index
         @produtos = Produto.all
     end
 
     def show
+    end
+
+    def setProduto
         @produto = Produto.find(params[:id])
     end
 end
